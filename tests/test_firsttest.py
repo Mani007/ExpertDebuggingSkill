0c648ef3c889774b86d7325dae36966b6e54e101
Mohnish <mohnish.a007@gmail.com>
2024-09-27 01:49:02 +0100

    First test assertion fails

--- tests/test_firsttest.py
+++ tests/test_firsttest.py
@@ -3,3 +3,8 @@ import testingwork.firsttest as firsttest
 def test_add():
     assert firsttest.adding(2, 3) == 5
     assert firsttest.adding(-1, -2) == -3
+    assert firsttest.adding(-1, -4) == -3  
+def test_subtracting():
+    assert firsttest.subtracting(5, 3) == 2
+    assert firsttest.subtracting(-2, -1) == -1
+    assert firsttest.subtracting(-2, 1) == -3
