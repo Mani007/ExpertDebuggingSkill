03082f7c5b16c7fa6d93a58b6f395830c1b0fdec
Mohnish <mohnish.a007@gmail.com>
2024-09-27 22:57:29 +0100

    Testing classes using pytest using classes with test cases failing

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,6 +1,10 @@
 import pytest 
 import testingwork.shapes_class as CircleShape
 
-def test_circle_area():
-    circle = CircleShape.Circle(5)
-    assert circle.calculate_area() == 78.53981633974483
+# def test_circle_area():
+#     circle = CircleShape.Circle(5)
+#     assert circle.calculate_area() == 78.53981633974483
+class TestCircleShape:
+    def test_circle_area(self):
+        circle = CircleShape.Circle(5)
+        assert circle.calculate_area() == pytest.approx(78.53981633974483)
