a54e98169b3f55b0981d3360133665904a97dd43
Mohnish <mohnish.a007@gmail.com>
2024-09-27 23:04:34 +0100

    Testing classes using pytest using class methods setup and teardown

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -5,6 +5,12 @@ import testingwork.shapes_class as CircleShape
 #     circle = CircleShape.Circle(5)
 #     assert circle.calculate_area() == 78.53981633974483
 class TestCircleShape:
+
+    def setup_method(self,method):
+        print(f"Starting setup method {method}")
+    
+    def teardown_method(self, method):
+        print(f"Ending teardown method {method}")
     def test_circle_area(self):
         # circle = CircleShape.Circle(5)
         # assert circle.calculate_area() == pytest.approx(78.53981633974483)
