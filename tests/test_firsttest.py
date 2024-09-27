46f1d99d3a329356b4268fdc84a4fe18a29d52a3
Mohnish <mohnish.a007@gmail.com>
2024-09-27 22:06:32 +0100

    Test getting passed for zero division error

--- tests/test_firsttest.py
+++ tests/test_firsttest.py
@@ -3,8 +3,16 @@ import testingwork.firsttest as firsttest
 def test_add():
     assert firsttest.adding(2, 3) == 5
     assert firsttest.adding(-1, -2) == -3
-    assert firsttest.adding(-1, -4) == -3  
+    assert firsttest.adding(-1, -4) == -5  
 def test_subtracting():
     assert firsttest.subtracting(5, 3) == 2
     assert firsttest.subtracting(-2, -1) == -1
     assert firsttest.subtracting(-2, 1) == -3
+def test_dividing():
+    assert firsttest.dividing(6, 2) == 3
+    assert firsttest.dividing(-6, 2) == -3
+    assert firsttest.dividing(6, -2) == -3
+    with pytest.raises(ZeroDivisionError):
+        firsttest.dividing(6, 0)
+        firsttest.dividing(0, 6)
+        firsttest.dividing(-6, 0)
