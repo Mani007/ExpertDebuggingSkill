d86040084994f8dbed35f256e4251c731ce20247
Mohnish <mohnish.a007@gmail.com>
2024-10-20 00:11:17 +0100

    Software debugging: Introducing some more bugs

--- example1/first.py
+++ example1/first.py
@@ -19,8 +19,17 @@ def html_parser(s):
 
 html_input1 = '<bold>somehting</bold>'
 html_input2 = '<bold herf=">">somehting</bold>'
+html_input3 = '"<bold>somehting</bold>"'  # some more buggy inputs , clearly fails the test
+html_input4 = '<bold>"somehting"</bold>' # some more buggy inputs
+html_input5 = '<"bold">somehting<"/bold">' # some more buggy inputs
 
 parsed_output1 = html_parser(html_input1)
 print(parsed_output1)
 parsed_output2 = html_parser(html_input2)
 print(parsed_output2)
+parsed_output3 = html_parser(html_input3)
+print(parsed_output3)
+parsed_output4 = html_parser(html_input4)
+print(parsed_output4)
+parsed_output5 = html_parser(html_input5)
+print(parsed_output5)
