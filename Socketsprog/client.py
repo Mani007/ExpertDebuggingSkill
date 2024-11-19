b366ed17e78a6873747fdff8edbd89b7b989a494
Mohnish <mohnish.a007@gmail.com>
2024-11-19 01:47:02 +0000

    loop over receiver buffer

--- Socketsprog/client.py
+++ Socketsprog/client.py
@@ -1,5 +1,7 @@
 import socket
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 3434))
-msg = s.recv(16)
-print(msg.decode('utf-8'))
+s.connect((socket.gethostname(), 3434)) 
+
+while True:
+    msg = s.recv(16)
+    print(msg.decode('utf-8'))
